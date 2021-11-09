7e4251fe9c6c80d95a579f1b29faac95713ee2b6
revanth <revanth8187@gmail.com>
2021-11-09 20:19:58 +0530

    updated backend index file

---
+++ BackEnd/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/Laundry');
+const cors = require('cors'); 
+const bodyParser = require("body-parser"); 
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken") 
+const app = express();
+app.use(bodyParser ());
+app.use(cors())
+const dayjs = require("dayjs");
+
+const loginroutes=require("./routes/user");
+const orders = require('./routes/order')
+
+
+app.use("/order", async function(req,res,next){
+    try{
+        const token=await req.headers.authorization?.split(" ")[1];
+    console.log(token) 
+    if(!token){
+        return res.status(401).json({
+            status:"failed",
+            message:"not authenticated"
+        })
+    }
+    const decoded=jwt.verify(token,'secret');
+    if(!decoded){
+        return res.status(401).json({
+            status:"failed",
+            message:"token invalid"
+        })
+    }
+    req.user=decoded.data
+    }catch(e){
+        return res.status(500).json({
+            status:"failed",
+            message:e.message 
+        })
+    }
+    
+    next();
+})
+
+app.use("/",loginroutes);
+app.use("/",orders);
+
+app.listen(5000, ()=>console.log("server started")) 
+
