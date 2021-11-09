392c900c66af2104c27cbaa26cea8111d5bd6b89
revanth <revanth8187@gmail.com>
2021-11-09 21:12:50 +0530

    updated data

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route, Switch} from 'react-router-dom';
+import signup from './components/signup/signup';
+import ExistingOrders from './components/existingorders/existingorder';
+import Orderpage from "./components/screens/orderPage";
+import Creatingorder from "./components/screens/createOrder";
+import login from './components/login/login'
+import PrivateRoute from './components/utils/privaterouter'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( 
+    <BrowserRouter> 
+    <div className="App" >
+        <Switch> 
+        <Route exact path="/signup" component={signup} />
+        <Route exact path="/" component={login} />
+        <PrivateRoute exact path="/creatingorder" component={Creatingorder} /> 
+        <PrivateRoute exact path="/orderPage" component={Orderpage} /> 
+        <PrivateRoute exact path="/order" component={ExistingOrders} />
+        </Switch> 
+      </div>
+      </BrowserRouter> 
+)
 }
 
 export default App;
